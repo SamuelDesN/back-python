7008e8156f56cb58294215d79a9b77f256d26b70
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-31 00:01:05 +0100

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -59,3 +59,10 @@ def home():
 
 # Export the app as "handler" for Vercel
 handler = app
+
+def handler(request):
+    name = request.args.get('name', 'World')
+    return {
+        "statusCode": 200,
+        "body": f"Hello, {name}!"
+    }
