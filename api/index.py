81f9c604205355660ab29e50eb4b1840d5adefbb
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-30 23:42:05 +0100

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -10,31 +10,48 @@ client = MongoClient("mongodb+srv://Admin:Abc123.@cluster0.4ruo4.mongodb.net/")
 db = client['express']
 users_collection = db['usuarios']
 
-def user_to_json(user):
-    return {
-        "id": user.get("id", str(user["id"])),
-        "nombre": user["nombre"],
-        "apellido": user["apellido"],
-        "telefono": user["telefono"]
-    }
+#def user_to_json(user):
+#    return {
+#        "id": user.get("id", str(user["id"])),
+#        "nombre": user["nombre"],
+#        "apellido": user["apellido"],
+#        "telefono": user["telefono"]
+#    }
+#@app.route('/api/users', methods=['GET'])
+#def get_users():
+#    users = list(users_collection.find())
+#    return jsonify([user_to_json(user) for user in users])
+#@app.route('/api/users/<string:user_id>', methods=['GET'])
+#def get_user_by_id(user_id):
+#    user = users_collection.find_one({"id": user_id}) 
+#    if user:
+#        return jsonify(user_to_json(user))
+#    return jsonify({"error": "Usuario no encontrado"}), 404
+#
+#@app.route('/api/users', methods=['POST'])
+#def add_user():
+#    user = request.get_json()
+#    if not user or not all(key in user for key in ["nombre", "apellido", "telefono"]):
+#        return jsonify({"error": "Datos de usuario incompletos"}), 400
+#    
+#    result = users_collection.insert_one(user)
+#    return jsonify({"id": str(result.inserted_id)}), 201
+
 @app.route('/api/users', methods=['GET'])
 def get_users():
     users = list(users_collection.find())
-    return jsonify([user_to_json(user) for user in users])
-@app.route('/api/users/<string:user_id>', methods=['GET'])
-def get_user_by_id(user_id):
-    user = users_collection.find_one({"id": user_id}) 
-    if user:
-        return jsonify(user_to_json(user))
-    return jsonify({"error": "Usuario no encontrado"}), 404
+    return jsonify([{
+        "id": user["id"],
+        "nombre": user["nombre"],
+        "apellido": user["apellido"],
+        "telefono": user["telefono"]
+    } for user in users])
 
-@app.route('/api/users', methods=['POST'])
-def add_user():
-    user = request.get_json()
-    if not user or not all(key in user for key in ["nombre", "apellido", "telefono"]):
-        return jsonify({"error": "Datos de usuario incompletos"}), 400
-    
-    result = users_collection.insert_one(user)
-    return jsonify({"id": str(result.inserted_id)}), 201
+@app.route('/')
+def home():
+    return "API en Vercel funcionando correctamente"
+
+# Exportar la app como "handler"
+handler = app
 
 app.run()
