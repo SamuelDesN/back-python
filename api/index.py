98537a917647538a2f7817f828476845dcc61b17
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-30 23:34:16 +0100

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -1,27 +1,31 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pymongo import MongoClient
+from bson import ObjectId
+
 app = Flask(__name__)
-users = [
-    {"id": 1, "nombre": "Juan", "apellido": "Perez", "telefono": "987654321"},
-    {"id": 2, "nombre": "Maria", "apellido": "Fernandez", "telefono": "9708654321"},
-    {"id": 3, "nombre": "Pedro", "apellido": "Alvarez", "telefono": "987654321"},
-    {"id": 4, "nombre": "Marcos", "apellido": "Silva", "telefono": "987123654"}
-]
-CORS(app)  
+CORS(app)
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({"message": "🦄🌈✨👋🌎🌍🌏✨🌈🦄"})
+client = MongoClient("mongodb+srv://Admin:Abc123.@cluster0.4ruo4.mongodb.net/")
+db = client['express']
+users_collection = db['usuarios']
 
+def user_to_json(user):
+    return {
+        "id": user.get("id", str(user["id"])),
+        "nombre": user["nombre"],
+        "apellido": user["apellido"],
+        "telefono": user["telefono"]
+    }
 @app.route('/api/users', methods=['GET'])
 def get_users():
-    return jsonify(users)
-
-@app.route('/api/users/<int:user_id>', methods=['GET'])
+    users = list(users_collection.find())
+    return jsonify([user_to_json(user) for user in users])
+@app.route('/api/users/<string:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
-    user = next((u for u in users if u['id'] == user_id), None)
+    user = users_collection.find_one({"id": user_id}) 
     if user:
-        return jsonify(user)
+        return jsonify(user_to_json(user))
     return jsonify({"error": "Usuario no encontrado"}), 404
 
 @app.route('/api/users', methods=['POST'])
@@ -30,7 +34,7 @@ def add_user():
     if not user or not all(key in user for key in ["nombre", "apellido", "telefono"]):
         return jsonify({"error": "Datos de usuario incompletos"}), 400
     
-    user['id'] = len(users) + 1
-    users.append(user)
-    return jsonify(user), 201
+    result = users_collection.insert_one(user)
+    return jsonify({"id": str(result.inserted_id)}), 201
+
 app.run()
