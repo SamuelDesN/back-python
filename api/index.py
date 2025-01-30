4b04ed1cf695e28b8f35fd9a2d903f92bf70b4dc
SamuelDesN <vazquezsamuel877@gmail.com>
2025-01-31 00:49:53 +0100

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -4,23 +4,16 @@ from pymongo import MongoClient
 from bson import ObjectId
 
 app = Flask(__name__)
-
-handler = app
 CORS(app)
 
-try:
-    client = MongoClient("mongodb+srv://Admin:Abc123.@cluster0.4ruo4.mongodb.net/")
-    db = client['express']
-    users_collection = db['usuarios']
-    # Optional: Check if the connection is successful
-    client.admin.command('ping')
-except Exception as e:
-    print(f"Error connecting to MongoDB: {e}")
-    raise e  # Re-raise the exception to stop execution
+
+client = MongoClient("mongodb+srv://Admin:Abc123.@cluster0.4ruo4.mongodb.net/")
+db = client['express']
+users_collection = db['usuarios']
 
 def user_to_json(user):
     return {
-        "id": str(user["_id"]),  # Convert ObjectId to string
+        "id": str(user["id"]),
         "nombre": user["nombre"],
         "apellido": user["apellido"],
         "telefono": user["telefono"]
@@ -28,13 +21,23 @@ def user_to_json(user):
 
 @app.route('/api/users', methods=['GET'])
 def get_users():
-    try:
-        users = list(users_collection.find())
-        return jsonify([user_to_json(user) for user in users])
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    users = list(users_collection.find())
+    return jsonify([user_to_json(user) for user in users])
 
-@app.route('/')
-def home():
-    return "API en Vercel funcionando correctamente"
+@app.route('/api/users/<string:user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+    user = users_collection.find_one({"id":user_id})
+    if user:
+        return jsonify(user_to_json(user))
+    return jsonify({"error": "Usuario no encontrado"}), 404
 
+@app.route('/api/users', methods=['POST'])
+def add_user():
+    user = request.get_json()
+    if not user or not all(key in user for key in ["nombre", "apellido", "telefono"]):
+        return jsonify({"error": "Datos de usuario incompletos"}), 400
+    
+    result = users_collection.insert_one(user)
+    return jsonify({"id": str(result.inserted_id)}), 201
+
+app.run()
